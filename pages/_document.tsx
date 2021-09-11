8622d3dd1539f0b1d45fb1ff94e146b04ab7895c
halawata13 <rsuzuki13@gmail.com>
2021-09-11 15:11:40 +0900

    PWA対応

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,12 @@ export default class Document extends NextDocument {
       <Html>
         <Head>
           <title>Forecaster</title>
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="icon" type="image/png" href="/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'crossOrigin'} />
           <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet" />
+          <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />
